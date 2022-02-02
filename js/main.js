0e911d5265fce8f94287c8f303376d5b03b43b0a
Joshua Rain <1014054551@qq.com>
2022-02-01 23:40:53 -0500

    assignment0-website set up

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+
+$('#text-gt').click(function() {
+  $('#navigation a[href="#gt"]').tab('show');
+});
+
+$('#text-code').click(function() {
+  $('#navigation a[href="#code"]').tab('show');
+});
+})
